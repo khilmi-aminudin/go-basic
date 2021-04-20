5e3eec27f5eb3ced1e0b2306d0f66415a36567f2
khilmi-aminudin <khilmiaminudin715@gmail.com>
2021-04-20 11:56:24 +0700

    edit file functionAsParameter dan add file pointer.go

---
+++ functionAsParameter.go
@@ -0,0 +1,37 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+// )
+
+// func main() {
+// 	var data = []string{"wick", "jason", "ethan"}
+// 	var dataContainsO = filter(data, func(each string) bool {
+// 		return strings.Contains(each, "o")
+// 	})
+// 	var dataLenght5 = filter(data, func(each string) bool {
+// 		return len(each) == 5
+// 	})
+
+// 	fmt.Println("data asli \t\t:", data)
+// 	// data asli : [wick jason ethan]
+
+// 	fmt.Println("filter ada huruf \"o\"\t:", dataContainsO)
+// 	// filter ada huruf "o" : [jason]
+
+// 	fmt.Println("filter jumlah huruf \"5\"\t:", dataLenght5)
+// 	// filter jumlah huruf "5" : [jason ethan]
+// }
+
+// type FilterCallback func(string) bool
+
+// func filter(data []string, callback FilterCallback) []string {
+// 	var result []string
+// 	for _, each := range data {
+// 		if filtered := callback(each); filtered {
+// 			result = append(result, each)
+// 		}
+// 	}
+// 	return result
+// }
