b69ce9ae87f6b9922f8e44ee34463a9adcedbe73
khilmi-aminudin <khilmiaminudin715@gmail.com>
2021-04-21 09:36:39 +0700

    edit go mod

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module khilmi-aminudin/go-basic
 
 go 1.16
 
-require github.com/lib/pq v1.10.0 // indirect
+require github.com/lib/pq v1.10.0
---
+++ structMethod.go
@@ -0,0 +1,40 @@
+package main
+
+// import "fmt"
+
+// type person struct {
+// 	name string `nama lengkap`
+// 	age  int    `umur`
+// }
+
+// func (p person) sayHello() string {
+// 	return "Hallo?? nama saya " + p.name
+// }
+
+// // method dengan pointer struct
+// func (p *person) changeName(name string) {
+// 	p.name = name
+// }
+
+// func (p person) change(name string)  {
+// 	p.name = name
+// }
+
+// func main() {
+// 	p1 := person{
+// 		name: "khilmi",
+// 		age:  23,
+// 	}
+
+// 	p1.changeName("Fazza")
+
+// 	fmt.Println(p1.sayHello())
+
+// 	p1.changeName("Fathi")
+
+// 	fmt.Println(p1.sayHello())
+
+// 	p1.change("khilmi")
+
+// 	fmt.Println(p1.sayHello())
+// }
